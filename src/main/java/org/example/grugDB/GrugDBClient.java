843ab179d9bda5a850f6c49313d1c0938838532e
tatuaua <tatu.niemikorpi@gmail.com>
2025-03-23 21:51:12 +0200

    improve var names, start batch insert

--- src/main/java/org/example/grugDB/GrugDBClient.java
+++ src/main/java/org/example/grugDB/GrugDBClient.java
@@ -5,97 +5,106 @@ import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 
 public class GrugDBClient {
 
-    private static GrugDBClient instance = null;
-    private static File dbDir = null;
-    private static final Logger log = LoggerFactory.getLogger(GrugDBClient.class);
+    private static GrugDBClient singletonInstance = null;
+    private static File databaseDirectory = null;
+    private static final Logger logger = LoggerFactory.getLogger(GrugDBClient.class);
 
     private GrugDBClient() {
-        log.info("GrugDBClient instance created");
+        logger.info("GrugDBClient instance created");
     }
 
-    public static GrugDBClient getInstance(boolean clearDatabase) {
-        if (instance != null) {
-            return instance;
+    public static GrugDBClient getInstance(boolean shouldClearDatabase) {
+        if (singletonInstance != null) {
+            return singletonInstance;
         }
-        dbDir = new File("grug_db");
-        if (clearDatabase && dbDir.exists()) {
-            for (File file : Objects.requireNonNull(dbDir.listFiles())) {
-                file.delete();
+        databaseDirectory = new File("grug_db");
+        if (shouldClearDatabase && databaseDirectory.exists()) {
+            for (File storedFile : Objects.requireNonNull(databaseDirectory.listFiles())) {
+                storedFile.delete();
             }
         }
-        if (!dbDir.exists()) {
-            dbDir.mkdirs();
+        if (!databaseDirectory.exists()) {
+            databaseDirectory.mkdirs();
         }
-        instance = new GrugDBClient();
-        return instance;
+        singletonInstance = new GrugDBClient();
+        return singletonInstance;
     }
 
-    synchronized public <T> void save(T object) throws IOException {
-        String className = object.getClass().getSimpleName().toLowerCase();
-        File file = new File(dbDir, "grug_" + className + ".ser");
+    synchronized public <T> void save(T entity) throws IOException {
+        String entityTypeName = entity.getClass().getSimpleName().toLowerCase();
+        File storageFile = new File(databaseDirectory, "grug_" + entityTypeName + ".ser");
 
-        List<T> collection = loadCollection(file, className);
-        collection.add(object);
+        List<T> entityList = loadCollection(storageFile, entityTypeName);
+        entityList.add(entity);
 
-        try (FileOutputStream fos = new FileOutputStream(file);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(collection);
+        try (FileOutputStream fileOutput = new FileOutputStream(storageFile);
+             ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput)) {
+            objectOutput.writeObject(entityList);
         }
     }
 
-    synchronized public <T> List<T> find(Class<T> clazz) throws IOException {
-        String className = clazz.getSimpleName().toLowerCase();
-        File file = new File(dbDir, "grug_" + className + ".ser");
-        return loadCollection(file, className);
+    synchronized public <T> void saveBatch(List<T> entities) throws IOException {
+        String entityTypeName = entities.get(0).getClass().getSimpleName().toLowerCase();
+        File storageFile = new File(databaseDirectory, "grug_" + entityTypeName + ".ser");
+
+        List<T> existingEntityList = loadCollection(storageFile, entityTypeName);
+        Collections.addAll()
     }
 
-    synchronized public <T> void delete(Class<T> clazz, Predicate<T> predicate) throws IOException {
-        String className = clazz.getSimpleName().toLowerCase();
-        File file = new File(dbDir, "grug_" + className + ".ser");
-        List<T> collection = loadCollection(file, className);
+    synchronized public <T> List<T> find(Class<T> entityClass) throws IOException {
+        String entityTypeName = entityClass.getSimpleName().toLowerCase();
+        File storageFile = new File(databaseDirectory, "grug_" + entityTypeName + ".ser");
+        return loadCollection(storageFile, entityTypeName);
+    }
 
-        var sizeBefore = collection.size();
-        collection.removeIf(predicate);
-        var sizeAfter = collection.size();
+    synchronized public <T> void delete(Class<T> entityClass, Predicate<T> condition) throws IOException {
+        String entityTypeName = entityClass.getSimpleName().toLowerCase();
+        File storageFile = new File(databaseDirectory, "grug_" + entityTypeName + ".ser");
+        List<T> entityList = loadCollection(storageFile, entityTypeName);
 
-        log.debug("Deleted {} objects of type {}", sizeBefore - sizeAfter, className);
+        int originalSize = entityList.size();
+        entityList.removeIf(condition);
+        int updatedSize = entityList.size();
 
-        if(collection.isEmpty()) {
-            file.delete();
-            log.debug("Collection empty, deleted file {}", file.getName());
+        logger.debug("Deleted {} objects of type {}", originalSize - updatedSize, entityTypeName);
+
+        if (entityList.isEmpty()) {
+            storageFile.delete();
+            logger.debug("Collection empty, deleted file {}", storageFile.getName());
             return;
         }
 
-        try (FileOutputStream fos = new FileOutputStream(file);
-             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
-            oos.writeObject(collection);
+        try (FileOutputStream fileOutput = new FileOutputStream(storageFile);
+             ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput)) {
+            objectOutput.writeObject(entityList);
         }
     }
 
     @SuppressWarnings("unchecked")
-    private <T> List<T> loadCollection(File file, String className) throws IOException {
-        if (!file.exists() || file.length() == 0) {
-            log.debug("No data found for {}, returning empty list", className);
+    private <T> List<T> loadCollection(File storageFile, String entityTypeName) throws IOException {
+        if (!storageFile.exists() || storageFile.length() == 0) {
+            logger.debug("No data found for {}, returning empty list", entityTypeName);
             return new ArrayList<>();
         }
 
-        try (FileInputStream fis = new FileInputStream(file);
-             ObjectInputStream ois = new ObjectInputStream(fis)) {
-            Object obj = ois.readObject();
-            if (obj instanceof List) {
-                return (List<T>) obj;
+        try (FileInputStream fileInput = new FileInputStream(storageFile);
+             ObjectInputStream objectInput = new ObjectInputStream(fileInput)) {
+            Object deserializedObject = objectInput.readObject();
+            if (deserializedObject instanceof List) {
+                return (List<T>) deserializedObject;
             } else {
-                log.warn("Corrupted data for {}, resetting to empty list", className);
+                logger.warn("Corrupted data for {}, resetting to empty list", entityTypeName);
                 return new ArrayList<>();
             }
         } catch (ClassNotFoundException e) {
-            log.error("Class not found while loading {}: {}", className, e.getMessage());
+            logger.error("Class not found while loading {}: {}", entityTypeName, e.getMessage());
             throw new IOException("Failed to deserialize collection", e);
         }
     }
